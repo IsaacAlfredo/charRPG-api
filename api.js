a60b91dd8385700bd6f7165900bf4f9afa2a8f51
Isaac Alfredo <isaacfreitas23@gmail.com>
2024-02-03 18:09:49 -0300

    get and post routes

---
+++ api.js
@@ -0,0 +1,32 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { createRequire } from "module";
+import { createChar } from "./func.js";
+
+const require = createRequire(import.meta.url);
+const characters = require("./characters.json");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.listen("3000");
+
+app.route("/").get((req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Content-Type", "application/json");
+  res.sendFile(path.join(__dirname, "characters.json"));
+});
+
+app.post("/create", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  createChar(req.body.character);
+  res.send(req.body);
+  console.log(characters);
+});
---
+++ func.js
@@ -0,0 +1,17 @@
+import { createRequire } from "module";
+import * as fs from "fs";
+
+const require = createRequire(import.meta.url);
+const characters = require("./characters.json");
+
+export function createChar(newChar) {
+  characters.characters.push(newChar);
+  const newCharacters = JSON.stringify(characters);
+
+  fs.writeFile("characters.json", newCharacters, (error) => {
+    if (error) {
+      console.error(error);
+      throw error;
+    }
+  });
+}
